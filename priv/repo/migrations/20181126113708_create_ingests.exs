40fc301666e9fe2a6e33177f16d61c0d1b4be3e8
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-11-26 16:16:08 +0100

    chore: add domain_id field in ingests table

--- priv/repo/migrations/20181126113708_create_ingests.exs
+++ priv/repo/migrations/20181126113708_create_ingests.exs
@@ -10,6 +10,7 @@ defmodule TdIe.Repo.Migrations.CreateIngests do
       add :last_change_by, :bigint, null: false
       add :last_change_at, :utc_datetime, null: false
       add :parent_id, references(:ingests), null: true
+      add :domain_id, :bigint, null: true
 
       timestamps(type: :utc_datetime)
     end
