b69acc9c7156c41a9fc14da8f5fef4f4c8de76ea
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-11-26 12:52:35 +0100

    feat: New migration creating ingests table

---
+++ priv/repo/migrations/20181126113708_create_ingests.exs
@@ -0,0 +1,17 @@
+defmodule TdIe.Repo.Migrations.CreateIngests do
+  @moduledoc """
+  Migration to create ingests tables
+  """
+  use Ecto.Migration
+
+  def change do
+    create table(:ingests) do
+      add :type, :string, null: false
+      add :last_change_by, :bigint, null: false
+      add :last_change_at, :utc_datetime, null: false
+      add :parent_id, references(:ingests), null: true
+
+      timestamps(type: :utc_datetime)
+    end
+  end
+end
