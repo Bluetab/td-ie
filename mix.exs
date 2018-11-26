da31191cda12228bc394e6904727738cfeceb5ed
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-11-26 16:14:42 +0100

    feat: Add new versions

--- mix.exs
+++ mix.exs
@@ -38,7 +38,24 @@ defmodule TdIe.Mixfile do
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:guardian, "~> 1.0"},
+      {:cowboy, "~> 1.0"},
+      {:cabbage, git: "https://github.com/eriosv/cabbage.git"},
+      {:httpoison, "~> 1.0"},
+      {:edeliver, "~> 1.4.5"},
+      {:distillery, "~> 1.5", runtime: false},
+      {:credo, "~> 0.9.3", only: [:dev, :test], runtime: false},
+      {:canada, "~> 1.0.1"},
+      {:ex_machina, "~> 2.1", only: :test},
+      {:corsica, "~> 1.0"},
+      {:phoenix_swagger, "~> 0.8.0"},
+      {:ex_json_schema, "~> 0.5"},
+      {:json_diff, "~> 0.1.0"},
+      {:csv, "~> 2.0.0"},
+      {:nimble_csv, "~> 0.3"},
+      {:codepagex, "~> 0.1.4"},
+      {:td_perms, git: "https://github.com/Bluetab/td-perms.git", tag: "2.8.1"},
+      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "2.8.0"}
     ]
   end
 
