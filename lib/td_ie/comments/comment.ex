547cf314f1936888ae247bb9b738f51cec9d65e8
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-11-27 16:12:31 +0100

    feat: new comments model

---
+++ lib/td_ie/comments/comment.ex
@@ -0,0 +1,24 @@
+defmodule TdIe.Comments.Comment do
+  @moduledoc false
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "comments" do
+    field :content, :string
+    field :resource_id, :integer
+    field :resource_type, :string
+    field :user, :map
+    field :created_at, :utc_datetime
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:content, :resource_id, :resource_type, :user, :created_at])
+    |> validate_required([:content, :resource_id, :resource_type, :user, :created_at])
+  end
+end
