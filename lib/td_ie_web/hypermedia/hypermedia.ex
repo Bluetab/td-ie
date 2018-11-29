dbf2c73eaa43b5c0b201af98230004fcaa3ad073
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-11-29 18:10:36 +0100

    feat: add hypermedia logic

---
+++ lib/td_ie_web/hypermedia/hypermedia.ex
@@ -0,0 +1,19 @@
+defmodule TdIeWeb.Hypermedia do
+  @moduledoc false
+  def controller do
+    quote do
+      import TdIeWeb.Hypermedia.HypermediaControllerHelper
+    end
+  end
+
+  def view do
+    quote do
+      import TdIeWeb.Hypermedia.HypermediaViewHelper
+    end
+  end
+
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
+
+end
---
+++ lib/td_ie_web/hypermedia/hypermedia_collection.ex
@@ -0,0 +1,5 @@
+defmodule TdIeWeb.Hypermedia.HypermediaCollection do
+  @moduledoc false
+
+  defstruct [:collection_hypermedia, :collection]
+end
