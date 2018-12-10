38da60d7d117c58d9390db8776e0c2d1562e3a19
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-12-10 09:45:20 +0100

    fix: invalid aliases

--- lib/td_ie/release_tasks.ex
+++ lib/td_ie/release_tasks.ex
@@ -25,7 +25,7 @@ defmodule TdIe.ReleaseTasks do
       # Start apps necessary for executing migrations
       Enum.each(@start_apps, &Application.ensure_all_started/1)
 
-      # Start the Repo(s) for TdDf
+      # Start the Repo(s) for TdIe
       #IO.puts "Starting repos.."
       Enum.each(@repos, &(&1.start_link(pool_size: 1)))
 
@@ -48,7 +48,7 @@ defmodule TdIe.ReleaseTasks do
 
     defp run_migrations_for(app) do
       IO.puts "Running migrations for #{app}"
-      Migrator.run(TdDf.Repo, migrations_path(app), :up, all: true)
+      Migrator.run(TdIe.Repo, migrations_path(app), :up, all: true)
     end
 
     defp migrations_path(app), do: Path.join([priv_dir(app), "repo", "migrations"])
