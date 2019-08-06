837d5729422ba8bd08cf1fc540f7a2b49160b944
ivlo <ivan.lopez@bluetab.net>
2019-08-06 12:50:02 +0200

    fix: [TD-2045] change file_size type to int8

---
+++ priv/repo/migrations/20190806100147_change_file_size_ingest_executions.exs
@@ -0,0 +1,9 @@
+defmodule TdIe.Repo.Migrations.ChangeFileSizeIngestExecutions do
+  use Ecto.Migration
+
+  def change do
+    alter table(:ingest_executions) do
+      modify :file_size, :bigint
+    end
+  end
+end
