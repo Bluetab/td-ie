3205c69df604d8be53bd3a71c1bcf7f19836f145
euclydes.young@bluetab.net <euclydes.young@bluetab.net>
2021-03-24 14:42:55 +0000

    [TD-3445] Postgres port configurable through 'DB_PORT' environment variable

--- config/releases.exs
+++ config/releases.exs
@@ -4,7 +4,8 @@ config :td_ie, TdIe.Repo,
   username: System.fetch_env!("DB_USER"),
   password: System.fetch_env!("DB_PASSWORD"),
   database: System.fetch_env!("DB_NAME"),
-  hostname: System.fetch_env!("DB_HOST")
+  hostname: System.fetch_env!("DB_HOST"),
+  port: System.get_env("DB_PORT", "5432") |> String.to_integer()
 
 config :td_ie, TdIe.Auth.Guardian, secret_key: System.fetch_env!("GUARDIAN_SECRET_KEY")
 
