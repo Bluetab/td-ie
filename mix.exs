e8eed37bf8200ddf2896d705dacacc3c5bef8220
Juan Carlos Pazos <jucapaman@gmail.com>
2024-07-18 14:15:58 +0200

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdIe.Mixfile do
       app: :td_ie,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "6.8.0-local"
+          nil -> "6.8.1-local"
           v -> v
         end,
       elixir: "~> 1.14",
@@ -65,11 +65,11 @@ defmodule TdIe.Mixfile do
       {:json_diff, "~> 0.1.0"},
       {:csv, "~> 2.4"},
       {:ex_machina, "~> 2.3", only: :test},
-      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "6.3.0"},
-      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", branch: "hotfix/td-6713"},
+      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "6.5.1"},
+      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "6.8.3"},
       {:td_hypermedia, git: "https://github.com/Bluetab/td-hypermedia.git", tag: "4.54.0"},
       {:td_cluster, git: "https://github.com/Bluetab/td-cluster.git", tag: "5.19.0"},
-      {:td_core, git: "https://github.com/Bluetab/td-core.git", branch: "hotfix/td-6713"},
+      {:td_core, git: "https://github.com/Bluetab/td-core.git", tag: "6.8.4"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:assertions, "~> 0.19", only: :test},
