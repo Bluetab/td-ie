1a46d673291c6f888668204c41c4bc53e4931b7b
Guillermo RN <guillermo.r.navarrete@bluetab.net>
2024-07-29 11:01:58 +0000

    Release 6.9.2

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdIe.Mixfile do
       app: :td_ie,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "6.9.1-local"
+          nil -> "6.9.2-local"
           v -> v
         end,
       elixir: "~> 1.14",
