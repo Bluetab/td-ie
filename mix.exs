dbd0431f960dc86b235874811ce46f9955e50b0e
saul.isern <saul.isern@bluetab.net>
2025-04-09 12:12:39 +0200

    chore: bump versions

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdIe.Mixfile do
       app: :td_ie,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "7.2.0-local"
+          nil -> "7.4.0-local"
           v -> v
         end,
       elixir: "~> 1.18",
@@ -59,8 +59,8 @@ defmodule TdIe.Mixfile do
       {:canada, "~> 2.0"},
       {:quantum, "~> 3.5.3"},
       {:csv, "~> 3.2.1"},
-      {:td_hypermedia, git: "https://github.com/Bluetab/td-hypermedia.git", tag: "7.0.0"},
-      {:td_core, git: "https://github.com/Bluetab/td-core.git", tag: "7.0.1"},
+      {:td_hypermedia, git: "https://github.com/Bluetab/td-hypermedia.git", tag: "7.4.0"},
+      {:td_core, git: "https://github.com/Bluetab/td-core.git", tag: "7.4.0"},
       {:credo, "~> 1.7.11", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4.5", only: :dev, runtime: false},
       {:ex_machina, "~> 2.8", only: :test},
